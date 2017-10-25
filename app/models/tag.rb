4a5d33afaa43d7d357b2896822c8827feb96d6ea
Evan Wappel <eww125@Evans-MacBook-Pro.local>
2017-10-25 13:43:22 -0700

    test Atom save

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ApplicationRecord
+  class Tag < ActiveRecord::Base
+    has_many :destinations 
+  end
 end
--- db/migrate/20171025201034_create_destinations.rb
+++ db/migrate/20171025201034_create_destinations.rb
@@ -1,7 +1,10 @@
-class CreateDestinations < ActiveRecord::Migration[5.1]
+class CreateDestinations < ActiveRecord::Migration
   def change
     create_table :destinations do |t|
-
+			t.string :name
+			t.string :image
+			t.string :description
+			t.references :tag
       t.timestamps
     end
   end
